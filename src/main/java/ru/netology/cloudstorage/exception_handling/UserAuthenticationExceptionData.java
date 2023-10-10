6dcdbf7449fe4af06e1871aea9885da000619bcf
Timur Cherniuvsky Maksimovich <120785772+TimurChernavsky@users.noreply.github.com>
2023-10-10 19:48:26 +0300

    Add files via upload

---
+++ src/main/java/ru/netology/cloudstorage/exception_handling/UserAuthenticationExceptionData.java
@@ -0,0 +1,20 @@
+package ru.netology.cloudstorage.exception_handling;
+
+/**
+ * @author VladSemikin
+ */
+
+public class UserAuthenticationExceptionData {
+    private String info;
+
+    public UserAuthenticationExceptionData() {
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
+}
